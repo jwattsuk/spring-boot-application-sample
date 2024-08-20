ddd4c06bfa02d2b26f75b32019275b085f1d8204
Jonny Watts <jonnywatts@dolly.local>
2024-08-20 22:19:22 +0100

    adding working build

--- sample-service-parent/sample-service/src/main/java/com/jwattsuk/sample/main/SampleServiceApplication.java
+++ sample-service-parent/sample-service/src/main/java/com/jwattsuk/sample/main/SampleServiceApplication.java
@@ -1,33 +1,16 @@
 package com.jwattsuk.sample.main;
 
-import com.jwattsuk.sample.exception.ServiceInitialiseException;
-import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.xml.XmlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.eclipse.jetty.server.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SampleServiceApplication {
     private static final Logger LOG = LoggerFactory.getLogger(SampleServiceApplication.class);
 
     public static void main(String[] args) throws Exception {
-
-        LOG.info("Starting Sample Service");
-
-        final var server = createFromXml();
-        server.start();
-
-    }
-
-    private static Server createFromXml() {
-        // To Do - Add logging config
-        LOG.info("Reading Jetty Configuration from jetty-sample-all.xml");
-        try (var resource = Resource.newResource(
-                SampleServiceApplication.class.getClassLoader().getResource("jetty-sample-all.xml"))) {
-            var config = new XmlConfiguration(resource);
-            return (Server) config.configure();
-        } catch (Exception e) {
-            throw new ServiceInitialiseException(e);
-        }
+        LOG.info("Starting Sample Service with process id {}", System.getProperty("PID"));
+        SpringApplication.run(SampleServiceApplication.class, args);
     }
 }
